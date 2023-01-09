f4938e4d64f56f890d5b9e24135f29e3f53f939c
beast06king <116450986+beast06king@users.noreply.github.com>
2023-01-09 22:59:48 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -11,13 +11,13 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID',"3369707"))
     API_HASH = str(getenv('API_HASH',"aec1fd7abdfec322c426961a570ef336"))
-    BOT_TOKEN = str(getenv('BOT_TOKEN',"5460906870:AAETG89iRYZ8nc3P4Lm69vnLAz2N--NpzaM"))
-    name = str(getenv('name', "Filestreambot-pro"))
+    BOT_TOKEN = str(getenv('BOT_TOKEN',"5726328717:AAFziqmaN24ns07g2KEUUdvV0j5-Fhu4OU0"))
+    name = str(getenv('name', "filetolinkbot"))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', "60"))
     WORKERS = int(getenv('WORKERS', "4"))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL',"-1001666406019"))
     PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', "0.0.0.0"))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', "18.117.98.198"))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1115053159").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
@@ -29,7 +29,7 @@ class Var(object):
     
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', "unaware-tomasina-phoenix06king.koyeb.app")) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN',"18.117.98.198:8080")) if not ON_HEROKU or getenv('FQDN',"18.117.98.198:8080") else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
